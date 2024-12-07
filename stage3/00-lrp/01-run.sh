5e0f1517f4ab3faf86671b73bcbeb4504cfcdcd6
Volker Mauel <volkermauel@gmail.com>
2024-12-07 14:27:39 +0100

    native repo

--- stage3/00-lrp/01-run.sh
+++ stage3/00-lrp/01-run.sh
@@ -10,24 +10,7 @@ EOF
 echo "setting static ip for non networked stuff"
 install -m 600 files/ethernet-eth0 "${ROOTFS_DIR}/etc/NetworkManager/system-connections/ethernet-eth0"
 
-mount -o bind /proc "${ROOTFS_DIR}/proc"
 echo "Installing Rabbitmq dependencies"
 on_chroot << EOF
-	 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove -y $pkg; done
-	  sudo apt-get update
-	sudo apt-get install ca-certificates curl
-	sudo install -m 0755 -d /etc/apt/keyrings
-	sudo curl -fsSL https://download.docker.com/linux/raspbian/gpg -o /etc/apt/keyrings/docker.asc
-	sudo chmod a+r /etc/apt/keyrings/docker.asc
-	
-	# Add the repository to Apt sources:
-	echo \
-	  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/raspbian \
-	  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-	  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-	sudo apt-get update -y
-
- 	 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-   	docker pull rabbitmq
-    	umount /proc
+	sudo apt-get update && apt-get upgrade -y && apt-get install -y rabbitmq-server && systemctl enable rabbitmq-server
 EOF
